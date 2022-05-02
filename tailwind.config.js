ebdc73aa93d3600c455b14edaece1aac19e68729
mike <mike.liu@jazzhipster.com.tw>
2022-05-02 19:17:40 +0800

    npx tailwindcss init

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '3rem',
+        lg: '6rem',
+        xl: '10rem',
+        '2xl': '20rem'
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
